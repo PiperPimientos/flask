6f99aedbc3ff64b0b9a5a4e321e98f2d7b751fc0
felipe <feliperestrepomeneses@gmail.com>
2022-01-25 19:09:06 -0500

    implementamos Flask-Wtforms para crear un loginform y renderear con bootstrap

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from ensurepip import bootstrap
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['TODO 1', 'TODO 2', 'TODO 3']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -23,9 +31,11 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
-        'todos': todos
+        'todos': todos,
+        'login_form': login_form
     }
     return render_template('hello.html', **context)
 
