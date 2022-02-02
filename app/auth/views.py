e72e956285e2bd6f2c242a547a9811a573ab67a9
felipe <feliperestrepomeneses@gmail.com>
2022-02-02 17:07:51 -0500

    agregamos el render del POST que nos permite ingresar el usuario y redirigirnos

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,22 @@
+from crypt import methods
 from app.forms import LoginForm
-from flask import render_template
+from flask import render_template, session, flash, redirect, url_for
 
 from . import auth
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    login_form = LoginForm()
     context = {
         'login_form': LoginForm()
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usuario registrado con exito')
+
+        return redirect(url_for('index'))
+
     return render_template('login.html', **context)
